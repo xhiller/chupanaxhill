9a037e45058f3c8a14a099bb4c9c9986f07c2a08
xhiller <97200470+xhiller@users.noreply.github.com>
2022-01-06 07:14:04 +0800

    Add files via upload

---
+++ Message.py
@@ -0,0 +1,47 @@
+
+# Message class. Initialize "self" to the message you want to be sent.
+# @author GG
+# @version 1.0
+#
+
+
+def fetch_text():
+
+    self = """
+THE SPIDER FOMO IS COMING!
+AND WITH GREAT FOMO COMES GREAT XPONSIBILITY!
+
+
+SPIDER TOKEN 🕷
+
+Symbol: $SPIDEY
+
+The spider token is built as a meme token with exclusive NFTs - unique digital collectibles.
+
+WL PRESALE ON PINKSALE - Wednesday 15th DEC 2021
+
+🔐 KYC with Pinksale
+💪 Doxxed CA DEV
+🔒 70% LP LOCKED
+🖼 Exclusive NFTs
+
+📈 1,000,000 TOTAL TOKEN SUPPLY
+
+
+Tokenomics
+
+3% - liquidity 
+6% - marketing 
+1% - dev
+
+I AM BULLISH 🚀
+
+TG COMMUNITY: t.me/spidertokens"""
+
+    return self
+
+
+def fetch_list():
+    channel_list = ["Roningamez_PH","defishillers","pancakegemsz","gemhuntersz","bscloverss","defigemchatt","validcryptolinks","shillyourtoken","pancakeswapspam",
+    "uniswapspam","wiseapess","cryptogemarmy","spamcryptolinks","to2themoonn","cryptobunn","shillingbunny","cryptoospamm"]
+    return channel_list
